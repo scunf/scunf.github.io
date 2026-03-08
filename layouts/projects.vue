3f89d4b3f0fc070df0c39180871389cbeb38d255
robocon <19589917063@163.com>
2026-03-09 06:58:18 +0800

    update content

---
+++ layouts/projects.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useFrontmatter, useSiteConfig } from 'valaxy'
+
+const siteConfig = useSiteConfig()
+const fm = useFrontmatter()
+</script>
+
+<template>
+  <YunLayoutWrapper>
+    <YunLayoutLeft />
+
+    <div flex="~ col">
+      <YunProjects />
+      <YunSponsor class="mt-4" />
+      <YunComment
+        v-if="siteConfig.comment.enable && fm.comment !== false"
+        class="max-w-4xl m-auto"
+      />
+    </div>
+
+    <YunLayoutRight />
+  </YunLayoutWrapper>
+</template>
+
