61a34f8a94a871a0ee2edce7c1bb45d60642a7a1
robocon <19589917063@163.com>
2026-03-09 15:11:19 +0800

    update content

---
+++ layouts/project.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useFrontmatter, useSiteConfig } from 'valaxy'
+
+const siteConfig = useSiteConfig()
+const frontmatter = useFrontmatter()
+</script>
+
+<template>
+  <YunLayoutWrapper>
+    <YunLayoutLeft />
+
+    <RouterView v-slot="{ Component }">
+      <component :is="Component">
+        <template #main-header-after>
+          <YunMainHeaderAfter />
+        </template>
+
+        <template #main-content-after>
+          <YunMainContentAfter />
+        </template>
+
+        <template #aside-custom>
+          <slot name="aside-custom" />
+        </template>
+      </component>
+    </RouterView>
+
+    <YunLayoutRight />
+  </YunLayoutWrapper>
+</template>
+
